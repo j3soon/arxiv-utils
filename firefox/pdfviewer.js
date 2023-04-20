461dec400986295ce9dceb1cb7f873ab07a2bb57
Johnson Sun <j3.soon777@gmail.com>
2023-04-20 23:29:55 +0800

    Refactor pdfviewer.js for Firefox

--- firefox/pdfviewer.js
+++ firefox/pdfviewer.js
@@ -1,6 +1,5 @@
-// TODO: Refactor
-var app = {};
-app.name = "[arXiv-utils]";
+// This script modifies the title of the PDF in the container once it has finished loading.
+
 // Regular expressions for parsing arXiv URLs.
 // Ref: https://info.arxiv.org/help/arxiv_identifier_for_services.html#urls-for-standard-arxiv-functions
 const ABS_REGEXP = /arxiv\.org\/abs\/(\S*?)\/*$/;
@@ -18,53 +17,45 @@ function getId(url) {
          match_pdf && match_pdf[1] ||
          match_ftp && match_ftp[2] && [match_ftp[1], match_ftp[2]].join('');
 }
-// Get the title asynchronously, call the callbacks with the id, the type, and the queried title as argument when request done (`callback(id, type, title, newTitle)`).
-app.getTitleAsync = function (id, type, callback) {
-  console.log(app.name, "Retrieving title through ArXiv API request...");
-  var request = new XMLHttpRequest();
-  request.open("GET", "https://export.arxiv.org/api/query?id_list=" + id);
-  request.onload = function () {
-    if (request.status === 200) {
-      var resp = request.responseText;
-      var parser = new DOMParser();
-      var xmlDoc = parser.parseFromString(resp, "text/xml");
-      // The first title is query string, second one is paper name.
-      var title = xmlDoc.getElementsByTagName("title")[1].innerHTML;
-      title = title.replace("\n", "").replace("  ", " ");
-      // Store paper info
-      // Modify the title to differentiate from abstract pages.
-      title = title + " | " + type;
-      callback(title);
-    } else {
-      console.log(app.name, "Error: ArXiv API request failed.");
-    }
-  };
-  request.send();
-}
-// Insert the title into the active tab.
-app.insertTitle = function (title) {
-  var elTitle = document.getElementById("title");
-  elTitle.innerText = title;
-}
-// Extract the pdf url from 'pdfviewer.html?target=<pdfURL>'.
-app.extractURL = function () {
-  var url = new URL(window.location.href);
-  return url.searchParams.get("target");
-}
-// Inject embedded PDF.
-app.injectPDF = function (url) {
-  console.log(app.name, "Injecting PDF: " + url);
-  var elPDF = document.createElement("iframe");
-  elPDF.setAttribute("src", url);
-  var elContainer = document.getElementById("container");
-  elContainer.appendChild(elPDF);
-}
-// Run this once.
-app.run = function () {
-  var pdfURL = app.extractURL();
-  var id = getId(pdfURL);
-  app.getTitleAsync(id, "PDF", app.insertTitle);
-  app.injectPDF(pdfURL);
+// Get article information through arXiv API asynchronously.
+// Ref: https://info.arxiv.org/help/api/user-manual.html#31-calling-the-api
+async function getArticleInfoAsync(id, pageType) {
+  console.log(LOG_PREFIX, "Retrieving title through ArXiv API request...");
+  const response = await fetch(`https://export.arxiv.org/api/query?id_list=${id}`);
+  if (!response.ok) {
+    console.error(LOG_PREFIX, "Error: ArXiv API request failed.");
+    return;
+  }
+  const xmlDoc = await response.text();
+  const parsedXML = new DOMParser().parseFromString(xmlDoc, 'text/xml');
+  // title[0] is query string, title[1] is paper name.
+  const title = parsedXML.getElementsByTagName("title")[1].textContent;
+  // Long titles will be split into multiple lines, with all lines except the first one starting with two spaces.
+  const escapedTitle = title.replace("\n", "").replace("  ", " ");
+  // TODO: May need to escape special characters in title?
+  const newTitle = `${escapedTitle} | ${pageType}`;
+  return {
+    escapedTitle,
+    newTitle,
+  }
 }
 
-app.run();
+async function mainAsync() {
+  // Extract the pdf url from 'pdfviewer.html?target=<pdfURL>'.
+  const url = new URL(window.location.href).searchParams.get("target");
+  const pageType = url.includes("abs") ? "Abstract" : "PDF";
+  const id = getId(url);
+  if (!id) {
+    console.error(LOG_PREFIX, "Error: Failed to get paper ID, aborted.");
+    return;
+  }
+  const articleInfo = await getArticleInfoAsync(id, pageType);
+  document.title = articleInfo.newTitle;
+  console.log(LOG_PREFIX, `Set document title to: ${articleInfo.newTitle}.`);
+  console.log(LOG_PREFIX, "Injecting PDF: " + url);
+  const elContainer = document.getElementById("container");
+  elContainer.innerHTML += `<iframe src="${url}"></iframe>`;
+}
+
+// Execute main logic.
+mainAsync();
