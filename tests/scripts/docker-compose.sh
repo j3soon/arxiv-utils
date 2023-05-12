f73e06c24ee6330b3e47a99f68f92a03cd143b31
Johnson Sun <j3.soon777@gmail.com>
2023-05-13 04:01:07 +0800

    Cache docker images for github actions

--- tests/scripts/docker-compose.sh
+++ tests/scripts/docker-compose.sh
@@ -4,6 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 if [[ "$1" == "up" ]]; then
     docker-compose -f $DIR/../docker-compose.yml up --build "${@:2}"
+elif [[ "$1" == "pull" ]]; then
+    docker-compose -f $DIR/../docker-compose.yml pull
 elif [[ "$1" == "down" ]]; then
     docker-compose -f $DIR/../docker-compose.yml down
 elif [[ "$1" == "shutdown" ]]; then
